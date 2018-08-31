bcf8bfc4bcc7adfecb4a52bcd14e0a9913b44d5e
LifeOfArvin <arvinmaryami@gmail.com>
2018-08-31 14:13:26 +0200

    update

---
+++ CRM/Scripts/Endeavor.Incident.DisplayRelevantAttachments.js
@@ -0,0 +1,229 @@
+﻿// References:
+/// <reference path="vsdoc/jquery_2.1.4.js" />
+/// <reference path="SDK.Rest.js" />
+/// <reference path="CrmFetchKit.js" />
+/// <reference path="Endeavor.Common.Data.js" />
+
+/*
+Authour:     Arvin Maryami
+Date:        2018-06-01
+Dependencies: 
+Called from: Mark Attachments button from Incident Email
+*/
+
+
+// Begin scoping 
+if (typeof (Endeavor) == "undefined") {
+    var Endeavor = {
+    };
+}
+
+if (typeof (Endeavor.Incident) == "undefined") {
+    Endeavor.Incident = {
+    };
+}
+
+if (typeof (Endeavor.Incident.RelevantAttachments) == "undefined") {
+    Endeavor.Incident.DisplayRelevantAttachments = {
+
+
+        
+        appendRow: function (table, attachment) {
+            var row = table.insertRow(table.rows.length);
+            row.className = "attachment-row";
+
+            //id of attachment in hidden form
+            var idInDiv = document.createElement('DIV');
+            $(idInDiv).addClass("hide");idInDiv.appendChild(document.createTextNode(attachment.AttachmentId));
+
+            //preview button (span)
+            var preview = document.createElement('SPAN');
+            preview.title = "Förhandsvisa";
+            $(preview).addClass("file-name");preview.appendChild(document.createTextNode(attachment.FileName));
+
+            //checkbox
+            var checkbox = document.createElement("INPUT"); checkbox.setAttribute("type", "checkbox");
+            $(checkbox).addClass("pointify");
+
+            //size in kb column
+            var size = document.createElement('DIV');
+            $(size).addClass("file-size");
+            size.appendChild(document.createTextNode(attachment.FileSize / 1024));
+
+            this.createCell(row.insertCell(0), checkbox);
+            this.createCell(row.insertCell(1), preview);
+            this.createCell(row.insertCell(2), size);
+            this.createCell(row.insertCell(3), idInDiv);
+        },
+        createCell: function (cell, content) {
+            cell.appendChild(content);
+        },
+
+        run_Waitme: function () {
+            $('body').waitMe({
+                effect: 'win8',
+                text: "Creating records..",
+                bg: 'rgba(255,255,255,0.7)',
+                color: '#000'
+            });
+        },
+
+        // Get the attachments
+        fetchAttachments: function () {
+            var incidentId = window.parent.Xrm.Page.data.entity.getId().replace(/[{}]/g,"");
+            if (incidentId){
+                var iMUrl = Endeavor.Common.Data.getOrganizationServiceEndpoint() + "edp_RelevantAttachmentSet?$top=50&$select=edp_mimeType,edp_attachmentId,edp_fileSize,edp_Name&$filter=edp_IncidentId eq ('" + incidentId + "')";
+                var relAttachResSet = Endeavor.Common.Data.fetchJSONResults(iMUrl);
+
+                if (relAttachResSet.length > 0) {
+                    var attachObj  = {};
+                    var table = document.getElementById("attachmentsTable");
+                    for (var i = 0; i < relAttachResSet.length; i++){
+                        attachObj.FileName = relAttachResSet[i].edp_Name;
+                        attachObj.FileSize = relAttachResSet[i].edp_fileSize;
+                        attachObj.AttachmentId = relAttachResSet[i].edp_attachmentId;                       
+                        this.appendRow(table, attachObj);
+                    }
+                    this.addListeners();
+                    this.weirdWrapFix();
+                }               
+            }
+        },
+        //do not touch
+        weirdWrapFix: function(){
+            $("#attachmentsTable td:nth-child(1)").addClass('box');
+        },
+        //download record
+        download: function (filename, id){
+            if (id){
+                !filename || filename == "" ? "Untitled" : false;
+
+                var iMUrl = Endeavor.Common.Data.getOrganizationServiceEndpoint() + "ActivityMimeAttachmentSet?$select=FileName,Body,MimeType&$filter=ActivityMimeAttachmentId eq (guid'" + id + "')";
+                var attachment = Endeavor.Common.Data.fetchJSONResults(iMUrl);
+
+                if (attachment.length > 0){
+                    var e = document.createElement("a");
+                    e.setAttribute("href", "data:" + attachment[0].MimeType + ";base64," + attachment[0].Body);
+                    e.setAttribute("download", filename);
+                    e.style.display = "none";
+                    document.body.appendChild(e);
+                    e.click();
+                    document.body.removeChild(e);
+                }
+            }
+        },
+        //delete a record
+        delete: function (id) {
+            var iMUrl = Endeavor.Common.Data.getOrganizationServiceEndpoint() + "edp_RelevantAttachmentSet?$select=edp_RelevantAttachmentId&$filter=edp_attachmentId eq ('" + id + "')";
+            var relAttach = Endeavor.Common.Data.fetchJSONResults(iMUrl);
+            if (relAttach.length > 0){  
+                for(var i = 0; i < relAttach.length; i++){
+                    SDK.REST.deleteRecord(
+                        relAttach[i].edp_RelevantAttachmentId,
+                        "edp_RelevantAttachment",
+                        function () {
+                        return;
+                        },
+                        this.errorHandler
+                    );
+                }              
+            }
+            
+        },
+        errorHandler: function (error) {
+            alert(error.message);
+            return false;
+        },
+        addListeners: function () {
+            $('#attachmentsTable tr').click(function (event) {
+                if (event.target.type !== 'checkbox' && event.target.tagName !== 'SPAN') {
+                    $(':checkbox', this).trigger('click');
+                }
+            });
+            $("input[type='checkbox']").change(function (e) {
+                if ($(this).is(":checked")) { //If the checkbox is checked
+                    $(this).closest('tr').addClass("highlight-row");
+                    //Add class on checkbox checked
+                } else {
+                    $(this).closest('tr').removeClass("highlight-row");
+                    //Remove class on checkbox uncheck
+                }
+            });
+            //select or deselect all
+            $(".select").click(function (){
+                var table = document.getElementById("attachmentsTable");
+                var count = 0;
+                for (var i = 0; i < table.rows.length; i++){
+                    if (table.rows[i].cells[0].children[0].checked) {
+                        count++;
+                    }
+                }
+                if (count == table.rows.length) {
+                    for (var i = 0; i < table.rows.length; i++){
+                        table.rows[i].cells[0].children[0].checked = false;                         
+                    }
+                }else {
+                    for (var i = 0; i < table.rows.length; i++){
+                        table.rows[i].cells[0].children[0].checked = true; 
+                    }
+                }
+            });
+            $(".drop-list").click(function () {
+                if ($(".drop-menu").hasClass("hide")){
+                    $(".drop-menu").removeClass("hide");
+                }else{
+                    $(".drop-menu").addClass("hide");
+                }
+            });
+            $("#download").click(function () {
+                var table = document.getElementById("attachmentsTable");
+                if (table){
+                    for (var i = 0; i < table.rows.length; i++){
+                        if (table.rows[i].cells[0].children[0].checked){
+                            var fileName = table.rows[i].cells[1].children[0].innerHTML;
+                            var id = table.rows[i].cells[3].children[0].innerHTML;
+                            Endeavor.Incident.DisplayRelevantAttachments.download(fileName, id);
+
+                        }
+                    }
+                }
+            });
+            $("#delete").click(function (){
+                var table = document.getElementById("attachmentsTable");
+                if (table){
+                    if (confirm("Vill du ta bort valda filer från den här listan? Filerna kommer fortfarande att finnas kvar under tillhörande e-post")) {
+                        for (var i = 0; i < table.rows.length; i++){
+                            if (table.rows[i].cells[0].children[0].checked){
+                                var id = table.rows[i].cells[3].children[0].innerHTML;
+                                Endeavor.Incident.DisplayRelevantAttachments.delete(id);
+                            }
+                        }
+                        location.reload();
+                    }
+                }
+            });
+            $(".file-name").click(function () {
+                var attachmentId = $(this).closest("tr").find('td:eq(3)').text();
+                var iMUrl = Endeavor.Common.Data.getOrganizationServiceEndpoint() + "ActivityMimeAttachmentSet?$top=50&$select=FileName,Body,MimeType&$filter=ActivityMimeAttachmentId eq (guid'" + attachmentId + "')";
+                var attachment = Endeavor.Common.Data.fetchJSONResults(iMUrl);
+                if (attachment.length > 0) {
+                    if (attachment[0].MimeType === "application/pdf") {
+                        let pdfWindow = window.open("")
+                        pdfWindow.document.write("<iframe width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(attachment[0].Body) + "'></iframe>");
+                    } else {
+                        var img = new Image();
+                        img.src = "data:" + attachment[0].MimeType + ";base64," + attachment[0].Body;
+
+                        while (img.width > 800 && img.height > 600) {
+                            img.width = img.width / 2;
+                            img.height = img.height / 2;
+                        }
+
+                        var wdv = window.open("", "_blank", "width=" + img.width + ", height="+ img.height);
+                        wdv.document.write(img.outerHTML);
+                    }
+                }
+            });
+        }
+    };
+}
---
+++ CRM/Scripts/Endeavor.Incident.RelevantAttachments.js
@@ -0,0 +1,184 @@
+﻿// References:
+/// <reference path="vsdoc/jquery_2.1.4.js" />
+/// <reference path="SDK.Rest.js" />
+/// <reference path="CrmFetchKit.js" />
+/// <reference path="Endeavor.Common.Data.js" />
+
+/*
+Authour:     Arvin Maryami
+Date:        2018-06-01
+Dependencies: 
+Called from: Mark Attachments button from Incident Email
+*/
+
+var Xrm = Xrm || window.parent.Xrm;
+
+// Begin scoping 
+if (typeof (Endeavor) == "undefined") {
+    var Endeavor = {
+    };
+}
+
+if (typeof (Endeavor.Incident) == "undefined") {
+    Endeavor.Incident = {
+    };
+}
+
+if (typeof (Endeavor.Incident.RelevantAttachments) == "undefined") {
+    Endeavor.Incident.RelevantAttachments = {
+
+        loadAttachments: function () {
+            emailId = this.getDataParam();
+            if (emailId) {
+                var iMUrl = Endeavor.Common.Data.getOrganizationServiceEndpoint() + "EmailSet?$select=email_activity_mime_attachment/ActivityMimeAttachmentId,email_activity_mime_attachment/FileName,email_activity_mime_attachment/FileSize,email_activity_mime_attachment/MimeType,email_activity_mime_attachment/ActivityMimeAttachmentIdUnique,email_activity_mime_attachment/ActivityId&$expand=email_activity_mime_attachment&$filter=ActivityId eq (guid'" + emailId[0] + "')";               
+                var attachmentsResultSet = Endeavor.Common.Data.fetchJSONResults(iMUrl);
+
+                if (attachmentsResultSet.length > 0) {
+                    var table = document.getElementById("attachmentsTable");
+                    for (var i = 0; i < attachmentsResultSet[0].email_activity_mime_attachment.results.length; i++) {
+                        //Create checkbox
+                        var checkbox = document.createElement("INPUT"); checkbox.setAttribute("type", "checkbox");
+                        //create preview button
+                        var preDiv = document.createElement("DIV"); $(preDiv).addClass("preview font");
+                        preDiv.value = attachmentsResultSet[0].email_activity_mime_attachment.results[i].ActivityMimeAttachmentId;
+                        preDiv.appendChild(document.createTextNode("Visa "));
+                        var preSpan = document.createElement("SPAN");
+                        $(preSpan).addClass("glyphicon glyphicon-zoom-in");
+                        preDiv.appendChild(preSpan);
+                        //append the row
+                        this.appendRow(table, attachmentsResultSet[0].email_activity_mime_attachment.results[i], checkbox, preDiv);
+                    } 
+                    this.addListeners();
+                  
+                }
+
+            } else {
+                alert("Was not able to fetch Email Id, please contact an administrator.")
+            }
+        },
+        appendRow: function (table, attachment, checkbox, preDiv) {
+            // Set preview button listener
+            var row = table.insertRow(table.rows.length);
+            row.className = "attachment-row";
+            this.createCell(row.insertCell(0), checkbox);
+            this.createCell(row.insertCell(1), document.createTextNode(attachment.FileName));
+            this.createCell(row.insertCell(2), document.createTextNode(Math.round(attachment.FileSize / 1024)));
+            this.createCell(row.insertCell(3), preDiv);
+        },
+        createCell: function (cell, content) {
+            cell.appendChild(content);
+        },
+        // On submit: take all checked rows and send it to createImportantRecords
+        onSubmit: function () {
+            // for each row loop
+            Endeavor.Incident.RelevantAttachments.run_Waitme();
+            var attachmentsTable = document.getElementById("attachmentsTable");
+            for (var i = 0; i < attachmentsTable.rows.length; i++) {
+                if (attachmentsTable.rows[i].cells[0].children[0].checked) {
+                    var col = attachmentsTable.rows[i];
+                    var attachment = {};
+                    attachment.edp_Name = col.cells[1].innerHTML;
+                    attachment.edp_fileSize = ((col.cells[2].innerHTML) * 1024).toString();
+                    attachment.edp_attachmentId = col.cells[3].children[0].value;
+                    this.createImportantAttachmentRecord(attachment, col)                 
+                }
+            }
+            this.sleep(500).then(() => {
+                $("body").waitMe('hide');
+            });
+        },
+        // create the relevant attachment records
+        createImportantAttachmentRecord: function (attachment) {
+            // create record using arrayobject 
+            var incidentId = this.getDataParam();
+            attachment.edp_IncidentId = incidentId[1].toString();
+            SDK.REST.createRecord(
+                 attachment,
+                 "edp_RelevantAttachment",
+                 function (attachment) {
+                     $("#status-header").removeClass('hide');
+                 },
+                this.errorHandler
+
+               );
+        },
+        
+        errorHandler: function (error) {
+            alert(error.message);
+            location.reload();
+            return false;
+        },
+
+        // Get passed values from btnOnClickMarkAttachments
+        getDataParam: function () {
+            // Get url string
+            // Split value from string
+            // parse value
+            var vals = new Array();
+            if (location.search != "") {
+                var query = decodeURIComponent(location.search);
+                vals = query.substr(6).split("&");
+                for (var i in vals) {
+                    vals[i] = vals[i].replace(/\+/g, " ").split("=");
+                }
+                //look for the parameter named 'data'
+                for (var i in vals) {
+                    if (vals[i][0]) {
+                        vals[i][0] = vals[i][0].slice(1, -1);
+                    }
+                }
+                return vals;
+            }
+        },
+
+        run_Waitme: function () {
+            $('body').waitMe({
+                effect: 'win8',
+                text: "Creating records..",
+                bg: 'rgba(255,255,255,0.7)',
+                color: '#000'
+            });
+        },
+
+        // sleep time expects milliseconds
+        sleep: function(time) {
+          return new Promise((resolve) => setTimeout(resolve, time));
+        },
+  
+        addListeners: function () {
+            $('#attachmentsTable tr').click(function (event) {
+                if (event.target.type !== 'checkbox' && event.target.tagName !== 'DIV') {
+                    $(':checkbox', this).trigger('click');
+                }
+            });
+            $("input[type='checkbox']").change(function (e) {
+                if ($(this).is(":checked")) { //If the checkbox is checked
+                    $(this).closest('tr').addClass("highlight-row");
+                    //Add class on checkbox checked
+                } else {
+                    $(this).closest('tr').removeClass("highlight-row");
+                    //Remove class on checkbox uncheck
+                }
+            });
+            $(".preview").click(function  () {
+                var attachmentId = this.value;
+                var div = document.getElementById("imageHolder");
+                var iMUrl = Endeavor.Common.Data.getOrganizationServiceEndpoint() + "ActivityMimeAttachmentSet?$top=50&$select=FileName,Body,MimeType&$filter=ActivityMimeAttachmentId eq (guid'" + attachmentId + "')";
+                var attachment = Endeavor.Common.Data.fetchJSONResults(iMUrl);
+                if (attachment.length > 0) {
+                    if (attachment[0].MimeType === "application/pdf") {
+                        let pdfWindow = window.open("")
+                        pdfWindow.document.write("<iframe width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(attachment[0].Body) + "'></iframe>")
+                    } else {
+                        var image = new Image();
+                        image.src = ("data:" + attachment[0].MimeType + ";base64," + attachment[0].Body);
+
+                        if (div.firstChild)
+                            div.removeChild(div.firstChild);
+                        div.appendChild(image);
+                    }
+                }              
+            });
+        }
+    };
+}
